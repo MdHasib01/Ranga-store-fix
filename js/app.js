d2002ed5b2acb07673f02c052c646094eb606256
MdHasib01 <md.hasibuzzaman001@gmail.com>
2021-09-02 17:40:41 +0600

    all done

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 const searchBook = ()=>{
     const searchField = document.getElementById('search-input');
     const searchText = searchField.value;
+
+
     // console.log(inputFieldText);
     searchField.value=''
     const url =`http://openlibrary.org/search.json?q=${searchText}`
@@ -16,12 +18,15 @@ const resultNone = displayStyle=>{document.getElementById('no-result').style.dis
 
 const displaySearch = booksData =>{
     const books=booksData.docs
+
         //search volume display
         const searchVolume = document.getElementById('search-volume')
         searchVolume.textContent=''
         const p = document.createElement('p')
         p.innerText=`Result Found: ${booksData.numFound}`;
         searchVolume.appendChild(p)
+
+
         //no result error
         if(booksData.numFound===0){
             resultNone('block')
@@ -31,20 +36,23 @@ const displaySearch = booksData =>{
             resultNone('none')
             document.getElementById('search-volume').style.display='block'
         }
+
+
         //clear search result
         const displayBookResult = document.getElementById('display-book');
         displayBookResult.textContent=''
+
+
     books.forEach(book => {
-        // console.log(book)
+        console.log(book)
        const displayBook = document.getElementById('display-book');
        const div = document.createElement('div');
        div.classList.add('book-card')
        div.innerHTML=`
        <img src="${`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}" alt="">
        <h3>Book Name: ${book.title}</h3>
-       <p>Author Name: ${book.author_namer}</p>
-       <p>Publisher: ${book.publisher}</p>
-       <p>Publish Year: ${book.publish_year}</p>
+       <p>Author Name: ${book.author_name?.[0]}</p>
+       <p>Publish Year: ${book.publish_year?.[0]}</p>
        `;
        displayBook.appendChild(div)
     });
